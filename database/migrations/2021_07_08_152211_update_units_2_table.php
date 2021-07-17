60fffe16eae13ff2f600bea4e245659f27b6a585
VijayKadiyam <freelancervijayk@gmail.com>
2021-07-17 16:25:02 +0530

    First commit

---
+++ database/migrations/2021_07_08_152211_update_units_2_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUnits2Table extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('units', function (Blueprint $table) {
+            $table->string('imei_number', 100)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
